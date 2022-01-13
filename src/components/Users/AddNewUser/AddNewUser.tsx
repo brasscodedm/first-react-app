113aaf3ca6efa1565e8d8b177539e19538ce7149
Dominik <d>
2022-01-13 20:23:39 +0100

    fix: precommit test

--- src/components/Users/AddNewUser/AddNewUser.tsx
+++ src/components/Users/AddNewUser/AddNewUser.tsx
@@ -4,9 +4,11 @@ import { useNavigate } from 'react-router-dom';
 
 export const AddNewUser = () => {
 	const navigate = useNavigate();
+
 	return (
 		<>
 			<div className={styles.bgMark} onClick={() => navigate('/users')} />
+
 			<div className={styles.wrapper}>
 				<div className={styles.closeBtn} onClick={() => navigate(-1)}>
 					BACK TO PREV ROUTE
